c9f959ba97d4744ab4363bd74338feed7ff593f7
Eduin <eduinnuevo@gmail.com>
2023-08-09 05:57:03 -0500

    agrego menu mobile

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 const menuEmail = document.querySelector(".email img");
 const desktopMenu= document.querySelector(".desktop-menu");
 menuEmail.addEventListener('click',toggledesktopMenu)
+
+const menuMobile = document.querySelector(".menu");
+const mobileMenu= document.querySelector(".mobile-menu");
+menuMobile.addEventListener('click',toggleMobileMenu)
+
 function toggledesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle('active-menu-mobile');
 }
