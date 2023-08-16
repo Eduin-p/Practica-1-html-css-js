8374b269449b795ce2f18f738699235d248bc9c5
Eduin <eduinnuevo@gmail.com>
2023-08-16 11:25:40 -0500

    mificicaciones para ocultar shoppingcart no funciona

--- js/main.js
+++ js/main.js
@@ -1,14 +1,22 @@
-const menuEmail = document.querySelector(".email img");
-const desktopMenu= document.querySelector(".desktop-menu");
-menuEmail.addEventListener('click',toggledesktopMenu)
+
+const menuEmail = document.querySelector(".email img");//donde se hace click
+const desktopMenu= document.querySelector(".desktop-menu"); // la clase que se modifica
+menuEmail.addEventListener('click',toggledesktopMenu)// funcion cuando se hace click
 
 const menuMobile = document.querySelector(".menu");
 const mobileMenu= document.querySelector(".mobile-menu");
 menuMobile.addEventListener('click',toggleMobileMenu)
 
+const menuCarrito = document.querySelector(".carrito");
+const asideMyorders= document.querySelector(".my-orders");
+menuCarrito.addEventListener('click',toggleAsideMyorders)
+
 function toggledesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
 function toggleMobileMenu(){
-    mobileMenu.classList.toggle('active-menu-mobile');
+    mobileMenu.classList.toggle('inactive');
+}
+function toggleAsideMyorders(){
+    asideMyorders.classList.toggle('inactive');
 }
