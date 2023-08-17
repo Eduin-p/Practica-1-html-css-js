028a02d4364a09d75e8b8cd9f762548c6646a714
Eduin <eduinnuevo@gmail.com>
2023-08-17 10:21:26 -0500

    se arreglo quitar y poner menus en js

--- js/main.js
+++ js/main.js
@@ -8,15 +8,39 @@ const mobileMenu= document.querySelector(".mobile-menu");
 menuMobile.addEventListener('click',toggleMobileMenu)
 
 const menuCarrito = document.querySelector(".carrito");
+const flechaCarrito = document.querySelector(".menu-container figure img");
 const asideMyorders= document.querySelector(".my-orders");
 menuCarrito.addEventListener('click',toggleAsideMyorders)
+flechaCarrito.addEventListener('click',toggleAsideMyorders)
 
 function toggledesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+    //pregunta si No tiene la clase
+    if (!mobileMenu.classList.contains('inactive')) {
+        mobileMenu.classList.add('inactive');
+    }
+    if (!asideMyorders.classList.contains('inactive')) {
+        asideMyorders.classList.add('inactive');
+    }
+    
 }
 function toggleMobileMenu(){
     mobileMenu.classList.toggle('inactive');
+     //pregunta si No tiene la clase
+     if (!desktopMenu.classList.contains('inactive')) {
+        desktopMenu.classList.add('inactive');
+    }
+    if (!asideMyorders.classList.contains('inactive')) {
+        asideMyorders.classList.add('inactive');
+    }
 }
 function toggleAsideMyorders(){
     asideMyorders.classList.toggle('inactive');
+     //pregunta si No tiene la clase
+     if (!mobileMenu.classList.contains('inactive')) {
+        mobileMenu.classList.add('inactive');
+    }
+    if (!desktopMenu.classList.contains('inactive')) {
+        desktopMenu.classList.add('inactive');
+    }
 }
