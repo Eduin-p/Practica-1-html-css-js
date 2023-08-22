a163c34e35ec161665ee8ca509eaf157929dc718
Eduin <eduinnuevo@gmail.com>
2023-08-22 17:15:23 -0500

    integro product detail, falta corregirlo

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ const flechaCarrito = document.querySelector(".menu-container figure img");
 const asideMyorders= document.querySelector(".my-orders");
 const cardsContainer= document.querySelector(".cards-container-carrito");
 const cardsContainerInicio= document.querySelector(".cards-container");
+const productDetail =document.querySelector(".product-detail");
 menuCarrito.addEventListener('click',toggleAsideMyorders)
 flechaCarrito.addEventListener('click',toggleAsideMyorders)
 
@@ -46,6 +47,11 @@ function toggleAsideMyorders(){
         desktopMenu.classList.add('inactive');
     }
 }
+function openProdutDetail(){
+    productDetail.classList.remove('inactive')
+}
+
+// no esta funcionando openproduct 
 const productList = [];
 productList.push({
     name: 'Bike',
@@ -55,137 +61,119 @@ productList.push({
 productList.push({
     name: 'Monitor',
     price: 150,
-    image: 'https://images.pexels.com/photos/1855349/pexels-photo-1855349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 })
 productList.push({
     name: 'Teclado',
     price: 12,
-    image: 'https://images.pexels.com/photos/1855349/pexels-photo-1855349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    image: 'https://images.pexels.com/photos/3829226/pexels-photo-3829226.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 })
-
-/* <div class="product-card-carrito">
-    <img src="https://images.pexels.com/photos/1855349/pexels-photo-1855349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Product">
-    <div class="product-info">
-        <p>Bici</p>
-        <p>$120</p>
-    </div> 
-    <figure class="delete">
-        <img src="./icons/icon_close.png" alt="Delete">
-     </figure>  
-</div> */
-
-for (product of productList){
-// estructura para crear los elementos de Html
-//--------------------------
-    const productCard =document.createElement('div');//crear el div
-    productCard.classList.add('product-card-carrito')//darle la clase
-
-    const img =document.createElement('img');
-    img.setAttribute('src',product.image)
-    productCard.appendChild(img);// meter los elementos en su padre
-
-// -----------------------------------------
-    const productInfo =document.createElement('div');
-    productInfo.classList.add('product-info')
-
-    const productPrice = document.createElement('p');
-    productPrice.classList.add('price')//darle la clase
-    productPrice.innerText='$' + product.price;
-
-    const productName = document.createElement('p');
-    productName.classList.add('name')//darle la clase
-    productName.innerText= product.name;
-
-    productInfo.append(productName,productPrice);// meter los elementos en su padre
-//--------------------
-    const productFigure = document.createElement('figure');
-    productFigure.classList.add('delete')
-
-    const imgFigure =document.createElement('img');
-    imgFigure.setAttribute('src','./icons/icon_close.png');
-
-    productFigure.appendChild(imgFigure);// meter los elementos en su padre
-//----------------------    
-productCard.append(productInfo,productFigure);// meter los elementos en su padre
-cardsContainer.appendChild(productCard)
-/* llame al div padre(cardsContainer) para poder meter
-el div de productCard
-*/
-//--------- fin de la creacion de los elementos Html
-
-}
-//-------------
-//creacion de cards de inicio
+// array cards de inicio
 const productListCardsInicio = [];
 productListCardsInicio.push({
     name: 'Bike',
-    price: 120,
+    price: 180,
     image: 'https://images.pexels.com/photos/1855349/pexels-photo-1855349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 })
 productListCardsInicio.push({
-    name: 'Monitor',
-    price: 150,
-    image: 'https://images.pexels.com/photos/1855349/pexels-photo-1855349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    name: 'Patines',
+    price: 50,
+    image: 'https://images.pexels.com/photos/914996/pexels-photo-914996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 })
 productListCardsInicio.push({
-    name: 'Teclado',
-    price: 12,
-    image: 'https://images.pexels.com/photos/1855349/pexels-photo-1855349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    name: 'Parlante',
+    price: 30,
+    image: 'https://images.pexels.com/photos/2651794/pexels-photo-2651794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
 })
 
-/*
-<div class="product-card">
-    <img src="https://images.pexels.com/photos/1855349/pexels-photo-1855349.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Product">
-    <div class="product-info">
-        <div >
-            <p>$120</p>
-            p>Bici</p>
-        </div>
-        <figure >
-             <img src="./icons/bt_add_to_cart.svg" alt="carrito">
-         </figure>
-    </div>   
-</div>
- */
-for (product of productListCardsInicio){
-    // estructura para crear los elementos de Html
-    //--------------------------
-        const productCard =document.createElement('div');//crear el div
-        productCard.classList.add('product-card')//darle la clase
-    
-        const img =document.createElement('img');
-        img.setAttribute('src',product.image)
-        productCard.appendChild(img);// meter los elementos en su padre
-    
-    // -----------------------------------------
-        const productInfo =document.createElement('div');
-        productInfo.classList.add('product-info')
+renderProductsCarrito(productList);//creacion de cards carrito
+function renderProductsCarrito(arr){
+    for (product of arr){
+        // estructura para crear los elementos de Html
+        //--------------------------
+            const productCard =document.createElement('div');//crear el div
+            productCard.classList.add('product-card-carrito')//darle la clase
+        
+            const img =document.createElement('img');
+            img.setAttribute('src',product.image)
+            productCard.appendChild(img);// meter los elementos en su padre
+        
+        // -----------------------------------------
+            const productInfo =document.createElement('div');
+            productInfo.classList.add('product-info')
+        
+            const productPrice = document.createElement('p');
+            productPrice.classList.add('price')//darle la clase
+            productPrice.innerText='$' + product.price;
+        
+            const productName = document.createElement('p');
+            productName.classList.add('name')//darle la clase
+            productName.innerText= product.name;
+        
+            productInfo.append(productName,productPrice);// meter los elementos en su padre
+        //--------------------
+            const productFigure = document.createElement('figure');
+            productFigure.classList.add('delete')
+        
+            const imgFigure =document.createElement('img');
+            imgFigure.setAttribute('src','./icons/icon_close.png');
+        
+            productFigure.appendChild(imgFigure);// meter los elementos en su padre
+        //----------------------    
+        productCard.append(productInfo,productFigure);// meter los elementos en su padre
+        cardsContainer.appendChild(productCard)
+        /* llame al div padre(cardsContainer) para poder meter
+        el div de productCard
+        */
+        //--------- fin de la creacion de los elementos Html
+        
+        }
+}
+renderProductsInicio(productListCardsInicio)
 
-        const productInfoDiv =document.createElement('div');
-    
-        const productPrice = document.createElement('p');
-        productPrice.classList.add('price')//darle la clase
-        productPrice.innerText='$' + product.price;
-    
-        const productName = document.createElement('p');
-        productName.classList.add('name')//darle la clase
-        productName.innerText= product.name;
-    
-        productInfoDiv.append(productPrice,productName);// meter los elementos en su padre
-    //--------------------
-        const productFigure = document.createElement('figure');
-    
-        const imgFigure =document.createElement('img');
-        imgFigure.setAttribute('src','./icons/bt_add_to_cart.svg');
-    
-        productFigure.appendChild(imgFigure);// meter los elementos en su padre
-    //----------------------    
-    productInfo.append(productInfoDiv,productFigure);// meter los elementos en su padre
-    productCard.appendChild(productInfo)
-    cardsContainerInicio.appendChild(productCard)
-    /* llame al div padre(cardsContainer) para poder meter
-    el div de productCard
-    */
-    //--------- fin de la creacion de los elementos Html
-    
-    }
+function renderProductsInicio(arr){//creacion de cards de inicio
+
+    for (product of productListCardsInicio){
+        // estructura para crear los elementos de Html
+        //--------------------------
+            const productCard =document.createElement('div');//crear el div
+            productCard.classList.add('product-card')//darle la clase
+        
+            const img =document.createElement('img');
+            img.setAttribute('src',product.image)
+            productCard.appendChild(img);// meter los elementos en su padre
+            img.addEventListener('click',openProdutDetail())
+        // -----------------------------------------
+            const productInfo =document.createElement('div');
+            productInfo.classList.add('product-info')
+
+            const productInfoDiv =document.createElement('div');
+        
+            const productPrice = document.createElement('p');
+            productPrice.classList.add('price')//darle la clase
+            productPrice.innerText='$' + product.price;
+        
+            const productName = document.createElement('p');
+            productName.classList.add('name')//darle la clase
+            productName.innerText= product.name;
+        
+            productInfoDiv.append(productPrice,productName);// meter los elementos en su padre
+        //--------------------
+            const productFigure = document.createElement('figure');
+        
+            const imgFigure =document.createElement('img');
+            imgFigure.setAttribute('src','./icons/bt_add_to_cart.svg');
+        
+            productFigure.appendChild(imgFigure);// meter los elementos en su padre
+        //----------------------    
+        productInfo.append(productInfoDiv,productFigure);// meter los elementos en su padre
+        productCard.appendChild(productInfo)
+        cardsContainerInicio.appendChild(productCard)
+        /* llame al div padre(cardsContainer) para poder meter
+        el div de productCard
+        */
+        //--------- fin de la creacion de los elementos Html
+        
+        }
+
+}
